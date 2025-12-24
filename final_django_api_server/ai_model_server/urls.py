688eb3c632f0da1f2932d34e460c1953051bf269
songyoungwoon <thdduddns98@naver.com>
2025-12-24 12:33:11 +0900

    feat add mosec api urls

---
+++ final_django_api_server/ai_model_server/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import AdministrationDashboardView, PatientRegistrationView
+
+urlpatterns = [
+    # path('mosec/nnU-Net-Seg', AdministrationDashboardView.as_view(), name='administration_dashboard'),
+]
