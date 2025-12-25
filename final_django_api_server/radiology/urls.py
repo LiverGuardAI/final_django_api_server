378f43917083097764a07bc0b327ab40a3af4c9d
songyoungwoon <thdduddns98@naver.com>
2025-12-25 19:11:51 +0900

    feat add filming start api

--- final_django_api_server/radiology/urls.py
+++ final_django_api_server/radiology/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RadiologyDashboardView, DICOMStudyListView, WaitlistView
+from .views import RadiologyDashboardView, DICOMStudyListView, WaitlistView, StartFilmingView
 
 urlpatterns = [
     path('dashboard/', RadiologyDashboardView.as_view(), name='radiology_dashboard'),
     path('studies/', DICOMStudyListView.as_view(), name='dicom_studies'),
     path('waitlist/', WaitlistView.as_view(), name='radiology_waitlist'),
+    path('waitlist/start-filming/', StartFilmingView.as_view(), name='start_filming'),
 ]
